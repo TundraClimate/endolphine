70bff0f65027c30a0707cf5fd611fe42b4cd552b
TundraClimate <tundra.tundere@gmail.com>
2024-12-21 02:26:03 +0900

    ✨feat: init ui thread

--- src/thread.rs
+++ src/thread.rs
@@ -1,10 +1,24 @@
 use crate::error::*;
-use std::sync::{atomic::AtomicBool, Arc};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use tokio::time::{self, Duration, Instant};
 
 pub async fn process(quit_flag: Arc<AtomicBool>) -> EpResult<()> {
+    loop {}
     Ok(())
 }
 
 pub async fn ui(quit_flag: Arc<AtomicBool>) -> EpResult<()> {
+    while !quit_flag.load(Ordering::Relaxed) {
+        let start = Instant::now();
+
+        let elapsed = start.elapsed();
+        if elapsed < Duration::from_millis(50) {
+            time::sleep(Duration::from_millis(50) - elapsed).await;
+        }
+    }
+
     Ok(())
 }
