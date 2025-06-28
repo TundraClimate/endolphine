e54deff34ac1d48f4a0b5dea534bea3223172057
TundraClimate <tundra.tundere@gmail.com>
2025-06-28 15:21:23 +0900

    ✨feat: Add key translater

--- src/event_manager.rs
+++ src/event_manager.rs
@@ -1,5 +1,6 @@
 use crossterm::event::{self, Event, KeyEvent};
 use tokio::task::JoinHandle;
+use viks::Key;
 
 pub fn spawn() -> JoinHandle<()> {
     tokio::spawn(async move {
@@ -14,8 +15,12 @@ pub fn spawn() -> JoinHandle<()> {
 }
 
 fn handle_key(key: KeyEvent) {
+    let Some(key) = translate_event_to_key(key) else {
+        return;
+    };
+
     // tmp
-    if key.code == event::KeyCode::Char('q') {
+    if key.to_string() == "Q" {
         crate::tui::disable();
 
         std::process::exit(0);
@@ -23,3 +28,134 @@ fn handle_key(key: KeyEvent) {
 }
 
 fn handle_resize(cols: u16, rows: u16) {}
+
+fn translate_event_to_key(key: KeyEvent) -> Option<Key> {
+    use crossterm::event::{KeyCode, KeyModifiers};
+
+    let mut key_str = match key.code {
+        KeyCode::Backspace => "BS",
+        KeyCode::Tab => "TAB",
+        KeyCode::Enter => "ENTER",
+        KeyCode::Esc => "ESC",
+        KeyCode::Char(' ') => "SPACE",
+        KeyCode::Char('!') => "!",
+        KeyCode::Char('"') => "\"",
+        KeyCode::Char('#') => "#",
+        KeyCode::Char('$') => "$",
+        KeyCode::Char('%') => "%",
+        KeyCode::Char('&') => "&",
+        KeyCode::Char('\'') => "'",
+        KeyCode::Char('(') => "(",
+        KeyCode::Char(')') => ")",
+        KeyCode::Char('*') => "*",
+        KeyCode::Char('+') => "+",
+        KeyCode::Char(',') => ",",
+        KeyCode::Char('-') => "-",
+        KeyCode::Char('.') => ".",
+        KeyCode::Char('/') => "/",
+        KeyCode::Char('0') => "0",
+        KeyCode::Char('1') => "1",
+        KeyCode::Char('2') => "2",
+        KeyCode::Char('3') => "3",
+        KeyCode::Char('4') => "4",
+        KeyCode::Char('5') => "5",
+        KeyCode::Char('6') => "6",
+        KeyCode::Char('7') => "7",
+        KeyCode::Char('8') => "8",
+        KeyCode::Char('9') => "9",
+        KeyCode::Char(':') => ":",
+        KeyCode::Char(';') => ";",
+        KeyCode::Char('<') => "lt",
+        KeyCode::Char('=') => "=",
+        KeyCode::Char('>') => ">",
+        KeyCode::Char('?') => "?",
+        KeyCode::Char('@') => "@",
+        KeyCode::Char('a') => "a",
+        KeyCode::Char('b') => "b",
+        KeyCode::Char('c') => "c",
+        KeyCode::Char('d') => "d",
+        KeyCode::Char('e') => "e",
+        KeyCode::Char('f') => "f",
+        KeyCode::Char('g') => "g",
+        KeyCode::Char('h') => "h",
+        KeyCode::Char('i') => "i",
+        KeyCode::Char('j') => "j",
+        KeyCode::Char('k') => "k",
+        KeyCode::Char('l') => "l",
+        KeyCode::Char('m') => "m",
+        KeyCode::Char('n') => "n",
+        KeyCode::Char('o') => "o",
+        KeyCode::Char('p') => "p",
+        KeyCode::Char('q') => "q",
+        KeyCode::Char('r') => "r",
+        KeyCode::Char('s') => "s",
+        KeyCode::Char('t') => "t",
+        KeyCode::Char('u') => "u",
+        KeyCode::Char('v') => "v",
+        KeyCode::Char('w') => "w",
+        KeyCode::Char('x') => "x",
+        KeyCode::Char('y') => "y",
+        KeyCode::Char('z') => "z",
+        KeyCode::Char('A') => "A",
+        KeyCode::Char('B') => "B",
+        KeyCode::Char('C') => "C",
+        KeyCode::Char('D') => "D",
+        KeyCode::Char('E') => "E",
+        KeyCode::Char('F') => "F",
+        KeyCode::Char('G') => "G",
+        KeyCode::Char('H') => "H",
+        KeyCode::Char('I') => "I",
+        KeyCode::Char('J') => "J",
+        KeyCode::Char('K') => "K",
+        KeyCode::Char('L') => "L",
+        KeyCode::Char('M') => "M",
+        KeyCode::Char('N') => "N",
+        KeyCode::Char('O') => "O",
+        KeyCode::Char('P') => "P",
+        KeyCode::Char('Q') => "Q",
+        KeyCode::Char('R') => "R",
+        KeyCode::Char('S') => "S",
+        KeyCode::Char('T') => "T",
+        KeyCode::Char('U') => "U",
+        KeyCode::Char('V') => "V",
+        KeyCode::Char('W') => "W",
+        KeyCode::Char('X') => "X",
+        KeyCode::Char('Y') => "Y",
+        KeyCode::Char('Z') => "Z",
+        KeyCode::Char('[') => "[",
+        KeyCode::Char('\\') => "\"",
+        KeyCode::Char(']') => "]",
+        KeyCode::Char('^') => "^",
+        KeyCode::Char('_') => "_",
+        KeyCode::Char('`') => "`",
+        KeyCode::Char('{') => "{",
+        KeyCode::Char('|') => "|",
+        KeyCode::Char('}') => "}",
+        KeyCode::Char('~') => "~",
+        KeyCode::Delete => "DEL",
+        _ => return None,
+    }
+    .to_string();
+
+    let is_big_alpha = key_str.len() == 1 && matches!(key_str.chars().next(), Some('A'..='Z'));
+
+    if !is_big_alpha && key.modifiers.contains(KeyModifiers::SHIFT) {
+        key_str = format!("s-{}", key_str);
+    } else if key.modifiers.contains(KeyModifiers::ALT) {
+        key_str = format!("a-{}", key_str);
+    } else if key.modifiers.contains(KeyModifiers::CONTROL) {
+        key_str = format!("c-{}", key_str);
+    }
+
+    let key_str = if key_str.len() > 1 {
+        format!("<{}>", key_str)
+    } else {
+        key_str
+    };
+
+    let Ok(key) = Key::new(&key_str) else {
+        panic!("event translate failed: code bug");
+    };
+
+    Some(key)
+}
