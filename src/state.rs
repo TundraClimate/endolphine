dbcf00578e2daacbe8c2247520221b5a71df2ac2
TundraClimate <tundra.tundere@gmail.com>
2025-06-30 12:07:25 +0900

    ✨feat: Add mode to State

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,18 @@
-use std::{path::PathBuf, sync::RwLock};
+use std::{
+    path::PathBuf,
+    sync::{RwLock, atomic::AtomicU8},
+};
 
 pub struct State {
     work_dir: WorkingDir,
+    mode: CurrentMode,
 }
 
 impl State {
     pub fn new(work_dir: PathBuf) -> State {
         Self {
             work_dir: WorkingDir::new(work_dir),
+            mode: CurrentMode::new(),
         }
     }
 }
@@ -18,8 +23,26 @@ struct WorkingDir {
 
 impl WorkingDir {
     fn new(path: PathBuf) -> Self {
-        WorkingDir {
+        Self {
             wd: RwLock::new(path),
+        }
+    }
+}
+
+#[repr(u8)]
+pub enum Mode {
+    Normal = 0,
+    Visual = 1,
+}
+
+struct CurrentMode {
+    now: AtomicU8,
+}
+
+impl CurrentMode {
+    fn new() -> Self {
+        Self {
+            now: AtomicU8::new(0),
         }
     }
 }
