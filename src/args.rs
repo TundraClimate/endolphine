4a1d678c0db50dd2ba53e79e4085c1448f68efe5
TundraClimate <tundra.tundere@gmail.com>
2025-07-12 00:50:28 +0900

    ✨feat: Add help messages

--- src/args.rs
+++ src/args.rs
@@ -4,17 +4,26 @@ use std::path::PathBuf;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    /// Endolphine opens in this directory, This path must be a directory
     #[arg(default_value = ".", value_parser = clap::value_parser!(PathBuf))]
     path: PathBuf,
 
-    #[arg(short = 'e')]
+    /// Open config file with $EDITOR
+    #[arg(short = 'e', long = "edit-config")]
     edit_config: bool,
 
+    /// Enable debug mode
     #[arg(long = "dbg")]
     dbg: bool,
 
-    #[arg(short = 'T', long)]
-    theme: Option<String>,
+    /// Download an unofficial theme from URL
+    #[arg(short = 'T', value_name = "URL")]
+    dl_theme_from_url: Option<String>,
+
+    /// Download an official theme.
+    /// The theme list is in the README#available-themes
+    #[arg(short = 't', value_name = "NAME")]
+    dl_theme_official: Option<String>,
 }
 
 pub enum Expected {
