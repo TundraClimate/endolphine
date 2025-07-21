7034e0aa5a70b112c2469f1f857ed5a9d61bf550
TundraClimate <tundra.tundere@gmail.com>
2025-07-21 14:36:06 +0900

    ✨feat: Impl the Yank command

--- src/proc/yank.rs
+++ src/proc/yank.rs
@@ -1,6 +1,62 @@
 use crate::state::State;
-use std::sync::Arc;
+use std::{io, path::Path, sync::Arc};
 
-pub fn yank(state: Arc<State>) {}
+pub fn clip_files<P: AsRef<Path>>(files: &[P]) -> io::Result<()> {
+    use crate::{clipboard, config};
 
-pub fn yank_selects(state: Arc<State>) {}
+    let files = files
+        .iter()
+        .map(|p| format!("file://{}\n", p.as_ref().to_string_lossy()))
+        .collect::<String>();
+
+    let conf = config::get();
+
+    if conf.native_cb {
+        clipboard::clip_native(files, "text/uri-list")
+    } else {
+        clipboard::clip(files)
+    }
+}
+
+pub fn yank(state: Arc<State>) {
+    use crate::misc;
+
+    let child_files = misc::sorted_child_files(&state.work_dir.get());
+
+    if let Some(target) = child_files.get(state.file_view.cursor.current()) {
+        if let Err(e) = clip_files(&[target]) {
+            crate::log!(
+                "Failed to yank the '{}': {}",
+                target.to_string_lossy(),
+                e.kind()
+            );
+        }
+    }
+}
+
+pub fn yank_selects(state: Arc<State>) {
+    use crate::{misc, proc::view};
+
+    let child_files = misc::sorted_child_files(&state.work_dir.get());
+    let selected = state.file_view.selection.collect();
+
+    let targets = child_files
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, c)| selected.contains(&i).then_some(c))
+        .collect::<Vec<_>>();
+
+    if let Err(e) = clip_files(&targets) {
+        crate::log!("Failed to yank {} files: {}", targets.len(), e.kind());
+    }
+
+    if !selected.is_empty() {
+        view::refresh(state.clone());
+
+        let cursor = &state.file_view.cursor;
+        let first = selected[0];
+
+        cursor.reset();
+        cursor.shift_p(first);
+    }
+}
