605cdb9205c42487f485710fd127002c4839ebf9
TundraClimate <tundra.tundere@gmail.com>
2025-08-09 11:45:59 +0900

    ✨feat: Impl Default to DeleteConfig

--- src/config/delete.rs
+++ src/config/delete.rs
@@ -1,8 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Default)]
+#[derive(Deserialize, Serialize)]
 pub(super) struct DeleteConfig {
     pub(super) listen_yes: bool,
     pub(super) put_to_temp: bool,
     pub(super) with_yank: bool,
+}
+
+impl Default for DeleteConfig {
+    fn default() -> Self {
+        Self {
+            listen_yes: true,
+            put_to_temp: false,
+            with_yank: false,
+        }
+    }
 }
