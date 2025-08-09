38a5ddfa9d59632dd0bcc2b012eb261e9e5301f6
TundraClimate <tundra.tundere@gmail.com>
2025-08-09 09:05:58 +0900

    ✨feat: Impl Default to PasteConfig

--- src/config/paste.rs
+++ src/config/paste.rs
@@ -1,8 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Default)]
+#[derive(Deserialize, Serialize)]
 pub(super) struct PasteConfig {
     pub(super) copied_suffix: String,
     pub(super) is_overwrite: bool,
     pub(super) listen_overwrite: bool,
+}
+
+impl Default for PasteConfig {
+    fn default() -> Self {
+        Self {
+            copied_suffix: String::from("_COPY"),
+            is_overwrite: false,
+            listen_overwrite: true,
+        }
+    }
 }
