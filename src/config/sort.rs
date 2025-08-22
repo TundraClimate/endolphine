684e638def93f8966bbeac638cbb257c0b588256
TundraClimate <tundra.tundere@gmail.com>
2025-08-22 21:25:13 +0900

    ✨feat: Impl sort type configuration

--- src/config/sort.rs
+++ src/config/sort.rs
@@ -1,28 +1,27 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-#[derive(Deserialize, Serialize)]
-pub(super) struct SortConfig {}
-
-impl Default for SortConfig {
-    fn default() -> Self {
-        Self {}
-    }
+#[derive(Deserialize, Serialize, Default)]
+pub(super) struct SortConfig {
+    types: Types,
 }
 
 impl SortConfig {
     pub(super) fn sort_func(&self) -> Box<dyn Fn(&mut [PathBuf]) + Send + Sync> {
         use crate::misc;
 
-        Box::new(move |files: &mut [PathBuf]| {
+        let ty = self.types;
+
+        Box::new(move |files| {
             files.sort_by_key(|path| {
                 let entry_name = misc::entry_name(path);
 
                 if &entry_name == ".ep.ed" {
-                    return (9999, entry_name.to_owned());
+                    return (255, 9999, entry_name.to_owned());
                 }
 
                 (
+                    ty.parse_type(path),
                     match entry_name.chars().next() {
                         Some(c) if c.is_lowercase() => 0,
                         Some(c) if c.is_uppercase() => 1,
@@ -33,5 +32,26 @@ impl SortConfig {
                 )
             })
         })
+    }
+}
+
+#[derive(Deserialize, Serialize, Clone, Copy, Default)]
+struct Types {
+    file: u8,
+    directory: u8,
+    symlink_file: u8,
+    symlink_dir: u8,
+    other: u8,
+}
+
+impl Types {
+    fn parse_type(&self, path: &Path) -> u8 {
+        match path {
+            path if path.is_symlink() && path.is_file() => self.symlink_file,
+            path if path.is_symlink() && path.is_dir() => self.symlink_dir,
+            path if path.is_dir() => self.directory,
+            path if path.is_file() => self.file,
+            _ => self.other,
+        }
     }
 }
